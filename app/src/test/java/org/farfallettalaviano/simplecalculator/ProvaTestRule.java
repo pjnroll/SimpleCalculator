aa9cb9c475280ec16be48f1d3cce10ecc281675b
stefanofarfa <stefanofarfa29@gmail.com>
2017-11-30 16:41:15 +0100

    added ProvaTestRule

---
+++ app/src/test/java/org/farfallettalaviano/simplecalculator/ProvaTestRule.java
@@ -0,0 +1,14 @@
+package org.farfallettalaviano.simplecalculator;
+
+/**
+ * Created by Stefano Farfalletta on 29/11/2017.
+ */
+
+import android.support.test.runner.AndroidJUnit;
+
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class ProvaTestRule {
+
+}
